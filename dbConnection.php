3987f8ace4d02e08e99b1949be1db4e61f53c0ac
vkacharya <vkacharya786@gmail.com>
2024-06-06 10:41:52 +0530

    add new data

--- dbConnection.php
+++ dbConnection.php
@@ -2,7 +2,7 @@
 $databaseHost = 'localhost';
 $databaseName = 'test';
 $databaseUsername = 'root';
-$databasePassword = 'root';
+$databasePassword = '';
 
 // Open a new connection to the MySQL server
 $mysqli = mysqli_connect($databaseHost, $databaseUsername, $databasePassword, $databaseName); 
